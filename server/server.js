85298ff7d2dcf1aa6fc27d8782ab7b5c5bb79e87
anju-kumaran <anju.naduth@gmail.com>
2022-01-04 14:52:14 -0500

    Added code to get all restaurants from db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
 require("dotenv").config();
 const express = require('express');
+const morgan = require("morgan");
+const db = require("./db");
 const app = express();
+
+app.use(express.json());
+
+// Get All Restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
