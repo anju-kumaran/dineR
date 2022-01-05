07aa71f53548da970add3fd65c9a1ed26089bf06
anju-kumaran <anju.naduth@gmail.com>
2022-01-04 21:32:25 -0500

    Update and Delete a restuarant

--- server/server.js
+++ server/server.js
@@ -58,6 +58,39 @@ app.post("/api/v1/restaurants", async(req, res) =>{
   }
 });
 
+//Update a restaurant
+app.put("/api/v1/restaurants/:id", async(req, res) => {
+  try{
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *",
+    [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      },
+    })
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+//Delete a restaurant
+app.delete("/api/v1/restaurants/:id", async (req, res) =>{
+  try{
+    const results = await db.query(
+      "DELETE FROM restaurants WHERE id = $1", 
+      [req.params.id]
+      );
+      res.status(204).json({
+        status: "success"
+      });
+  }catch(err){
+    console.log(err);
+  }
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
