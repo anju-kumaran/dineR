9d391e9b40794436c6c4c2e0aae7c25492768868
anju-kumaran <anju.naduth@gmail.com>
2022-01-04 21:05:02 -0500

    Create new restaurant

--- server/server.js
+++ server/server.js
@@ -40,6 +40,24 @@ app.get("/api/v1/restaurants/:id", async(req, res) =>{
   }
 });
 
+//Create a restaurant
+app.post("/api/v1/restaurants", async(req, res) =>{
+  console.log(req.body);
+  try{
+    const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *",
+    [req.body.name, req.body.location, req.body.price_range]);
+    console.log(results.rows[0]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      }
+    })
+  } catch(err) {
+    console.log(err);
+  }
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
