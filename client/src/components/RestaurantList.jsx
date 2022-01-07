ddad7a674b9d61c546bc2d07bf15615bb38a2a39
anju-kumaran <anju.naduth@gmail.com>
2022-01-07 13:47:52 -0500

    Used context api and rendered the restaurants list

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,17 +1,23 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useContext } from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
-const RestaurantList = () => {
+const RestaurantList = (props) => {
+  const { restaurants, setRestaurants } = useContext(RestaurantsContext);
 
-  useEffect(async () => {
-    try {
-      const response = await RestaurantFinder.get("/");
-      console.log(response);
-    } catch (err) {
-      
-    }
+  useEffect(() => {
     
-  }, [])
+    const fetchData = async () => {
+      try {
+            const response = await RestaurantFinder.get("/");
+            console.log(response.data.data.restaurants[0]);
+            setRestaurants(response.data.data.restaurants);
+            console.log('line no 15 : ',restaurants);
+          } catch (err) {}
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='list-group'>
@@ -27,47 +33,26 @@ const RestaurantList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Wendys</td>
-            <td>Toronto</td>
-            <td>$$</td>
-            <td>Rating</td>
-            <td>
-              <button className="btn btn-info">Edit</button>
-            </td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Pizzahut</td>
-            <td>Niagara</td>
-            <td>$$</td>
-            <td>Rating</td>
-            <td>
-              <button className="btn btn-info">Edit</button>
-            </td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>McDonalds</td>
-            <td>DownTown</td>
-            <td>$</td>
-            <td>Rating</td>
-            <td>
-              <button className="btn btn-info">Edit</button>
-            </td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-    
+          {restaurants && restaurants.map((restaurant) => {
+            return(
+              <tr key={restaurant.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{"$".repeat(restaurant.price_range)}</td>
+                <td>Rating</td>
+                <td>
+                  <button className="btn btn-info">Edit</button>
+                </td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            )
+          })}
        </tbody>
       </table>
     </div>
   )
 }
 
-export default RestaurantList
+export default RestaurantList;
