ae843594d8b0a54321c0f6425284033266f102f7
anju-kumaran <anju.naduth@gmail.com>
2022-01-12 00:29:27 -0500

    Fetch data for details page

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
+import { useParams } from 'react-router-dom';
 
 const RestaurantDetailPage = () => {
+  const {id} = useParams();
+  const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        console.log(response);
+        setSelectedRestaurant(response.data.data.restaurant);  
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
   return (
     <div>
-      detailspage
+      {selectedRestaurant.name}
     </div>
   )
 }
