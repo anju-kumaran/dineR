94dc7abfedd46cdbab9a6a4f43b8c2a32952ea72
anju-kumaran <anju.naduth@gmail.com>
2022-01-13 17:17:42 -0500

    Updated the query to find the count of reviews

--- server/server.js
+++ server/server.js
@@ -11,13 +11,17 @@ app.use(express.json());
 // Get All Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
   try {
-    const results = await db.query("SELECT * FROM restaurants");
-    console.log(results);
+    //const results = await db.query("SELECT * FROM restaurants");
+    const restaurantRatingsData = await db.query(
+      "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id"
+    );
+
+    console.log(restaurantRatingsData);
     res.status(200).json({
       status: "success",
-      results: results.rows.length,
+      results: restaurantRatingsData.rows.length,
       data: {
-        restaurants: results.rows,
+        restaurants: restaurantRatingsData.rows,
       },
     });
   } catch (err) {
@@ -29,7 +33,12 @@ app.get("/api/v1/restaurants", async (req, res) => {
 app.get("/api/v1/restaurants/:id", async(req, res) =>{
   console.log('id = ',req.params.id);
   try{
-    const restaurant = await db.query("SELECT * FROM restaurants WHERE id=$1", [req.params.id]);
+    //const restaurant = await db.query("SELECT * FROM restaurants WHERE id=$1", [req.params.id]);
+    const restaurant = await db.query(
+      "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1",
+      [req.params.id]
+    );
+
     console.log(restaurant.rows[0]);
 
     const reviews = await db.query("SELECT * FROM reviews WHERE restaurant_id=$1", [req.params.id]);
